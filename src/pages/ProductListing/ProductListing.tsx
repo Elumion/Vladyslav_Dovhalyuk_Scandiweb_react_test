72ebc06f3b4b023e61cc1c2994db2912f003f0fa
Elumion <dovgalyukvlad87@gmail.com>
2022-06-16 18:16:37 +0300

    small refactor

--- src/pages/ProductListing/ProductListing.tsx
+++ src/pages/ProductListing/ProductListing.tsx
@@ -1,34 +1,68 @@
 import React from "react";
 import { connect } from "react-redux";
+import { ProductCardProps } from "../../@types/ProductCardType";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import { fetchProducts } from "../../redux/ProductsReducer";
 import { withRouter } from "../../withRouter";
+import { CategoryName } from "./ProductsListing.styles";
 import { QUERY_PRODUCTS } from "./query";
 
 class ProductListing extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-  }
-  componentDidMount() {
+
     this.props.productsUpdate({
       query: QUERY_PRODUCTS,
       name: this.props.params.categoryName,
     });
+  }
+  componentDidMount() {
     // console.log(this.props, this.state);
   }
 
-  componentWillMount() {
-    this.props.productsUpdate({
-      query: QUERY_PRODUCTS,
-      name: this.props.params.categoryName,
-    });
+  componentWillMount() {}
+
+  shouldComponentUpdate() {
+    if (!this.props?.products) return true;
+    if (this.props.params.categoryName !== this.props.products.category.name) {
+      this.props.productsUpdate({
+        query: QUERY_PRODUCTS,
+        name: this.props.params.categoryName,
+      });
+      return true;
+    }
+    return false;
   }
 
   componentDidUpdate() {
     console.log(this.props.products);
   }
 
+  renderPhotos(photosArr: { products: ProductCardProps[] }) {
+    return photosArr?.products?.map((el) => (
+      <ProductCard
+        brand={el.brand}
+        gallery={el.gallery}
+        id={el.id}
+        inStock={el.inStock}
+        name={el.name}
+        prices={el.prices}
+        key={el.id}
+      />
+    ));
+  }
+
   render(): React.ReactNode {
-    return <div>{this.props.params.categoryName}</div>;
+    return (
+      <>
+        <CategoryName>
+          {`${this.props.params.categoryName[0].toUpperCase()}${this.props.params.categoryName.substr(
+            1
+          )}`}
+        </CategoryName>
+        <ul>{this.renderPhotos(this.props.products?.category)}</ul>
+      </>
+    );
   }
 }
 
