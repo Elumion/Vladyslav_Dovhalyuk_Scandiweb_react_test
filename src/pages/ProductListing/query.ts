7bb14d0ee27c67b1e10e035104c3fae168897758
Elumion <dovgalyukvlad87@gmail.com>
2022-06-16 18:17:16 +0300

    update query

--- src/pages/ProductListing/query.ts
+++ src/pages/ProductListing/query.ts
@@ -1,11 +1,22 @@
 import { gql} from "@apollo/client";
 
-export const QUERY_PRODUCTS = `
-query($name:CategoryInput!){
-    category(input:$name){
+export const QUERY_PRODUCTS = `query($name:CategoryInput){
+  category(input:$name){
+    name
+    products{
       name
-      products{
-        name
+      id
+      gallery
+      inStock
+      prices{
+        currency{
+          label
+          symbol
+        }
+        amount
       }
+      brand
     }
-}`
+  }
+    
+  }`
