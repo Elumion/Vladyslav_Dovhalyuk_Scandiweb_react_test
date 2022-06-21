c8912b1b5db90c08b76a75528e3965e236f5dc3e
Elumion <dovgalyukvlad87@gmail.com>
2022-06-21 16:50:55 +0300

    out of stock html and styles

--- src/components/ProductCard/ProductCard.styles.ts
+++ src/components/ProductCard/ProductCard.styles.ts
@@ -2,7 +2,34 @@ import styled from "@emotion/styled";
 
 export const StyledCard = styled.li`
     padding:16px;
+    position:relative;
+    .out-of-stock__sheet{
+        position:absolute;
+        background-color:#fff;
+        width:100%;
+        height:100%;
+        opacity:0.5;
+        top:0;
+        left:0;
+        z-index:3;
+    }
 
+    .card__upper-container{
+        position:relative;
+    }
+
+    .out-of-stock__text{
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 160%;
+        position:absolute;
+        top:50%;
+        left:50%;
+        text-transform:uppercase;
+        transform:translate(-50%,-50%);
+        z-index:2;
+    }
+    
     .card__img{
         width:354px;
         height:330px;
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -10,13 +10,19 @@ class ProductCard extends React.Component<ProductCardProps, any> {
   render(): React.ReactNode {
     return (
       <StyledCard key={this.props.id}>
-        <img
-          className="card__img"
-          src={this.props.gallery[0]}
-          alt={this.props.name}
-          width={354}
-          height={330}
-        />
+        {!this.props.inStock && <div className="out-of-stock__sheet"></div>}
+        <div className="card__upper-container">
+          {!this.props.inStock && (
+            <p className="out-of-stock__text"> Out of stock </p>
+          )}
+          <img
+            className="card__img"
+            src={this.props.gallery[0]}
+            alt={this.props.name}
+            width={354}
+            height={330}
+          />
+        </div>
         <p className="card__text">
           {this.props.brand} {this.props.name}
         </p>
