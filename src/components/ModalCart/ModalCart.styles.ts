47e7f2b36dddb52c7b6e5f641b245b4ea680a142
Elumion <dovgalyukvlad87@gmail.com>
2022-06-25 17:16:56 +0300

    refactor styles and scrollbar

--- src/components/ModalCart/ModalCart.styles.ts
+++ src/components/ModalCart/ModalCart.styles.ts
@@ -8,6 +8,42 @@ export const ModalCartContainer = styled.div`
     position:absolute;
     top:100%;
     right:0;
-    border:1px solid black;
-    z-index:10
-`
+    z-index:10;
+    
+
+
+    .bag{
+        font-size:16px;
+        line-height:25px;
+        margin: 0;
+    }
+     .bold-text{
+        font-weight:700;
+    }
+    .minicart__products-list{
+        display:flex;
+        flex-direction:column;
+        gap:40px;
+
+        max-height:60vh;
+        overflow-y:scroll;
+
+        ::-webkit-scrollbar-track {
+        border: 1px solid transparent;
+        padding: 2px 0;
+        background-color: transparent;
+        }
+
+        ::-webkit-scrollbar {
+            width: 4px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            border-radius: 10px;
+            box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+            background-color: #f1f1f1;
+            border: 1px solid #f1f1f1;
+            transform: rotate(180deg);
+        }
+    }
+    `
