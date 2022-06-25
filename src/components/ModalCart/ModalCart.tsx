432f1cec81370b3248c8f71860aca330b64fdf9e
Elumion <dovgalyukvlad87@gmail.com>
2022-06-25 17:17:48 +0300

    connect component to redux and implement logic and markup

--- src/components/ModalCart/ModalCart.tsx
+++ src/components/ModalCart/ModalCart.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { ProductCardProps } from "../../@types/ProductTypes";
+import FullProduct from "../FullProduct";
 import { ModalCartContainer } from "./ModalCart.styles";
 
 type Props = {
   show: boolean;
-  close: () => void;
+  products: ProductCardProps[];
   children?: JSX.Element | JSX.Element[] | string;
+  currency?: { label: string; symbol: string };
 };
 
 class ModalCart extends React.Component<Props, {}> {
@@ -12,13 +16,37 @@ class ModalCart extends React.Component<Props, {}> {
     super(props);
   }
 
+  renderProducts(products: ProductCardProps[]): React.ReactNode {
+    return this.props.products.map((product: ProductCardProps) => {
+      return (
+        <FullProduct
+          currency={{ label: this.props.currency?.label }}
+          product={product}
+          key={product.id}
+        />
+      );
+    });
+  }
+
   render(): React.ReactNode {
     return !this.props.show ? null : (
       <ModalCartContainer>
-        <p>TEST MODAL CART</p>
+        <p className="bag">
+          <span className="bold-text">My Bag,</span>{" "}
+          {this.props.products.length} items
+        </p>
+        <ul className="minicart__products-list">
+          {this.renderProducts(this.props.products)}
+        </ul>
       </ModalCartContainer>
     );
   }
 }
 
-export default ModalCart;
+function MapStateToProps(state: any) {
+  return {
+    currency: state.currency.data,
+  };
+}
+
+export default connect(MapStateToProps, null)(ModalCart);
