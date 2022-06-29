a563087b4056c4b92f91f64731e89ad149246b7d
Elumion <dovgalyukvlad87@gmail.com>
2022-06-29 23:24:14 +0300

    bug fix

--- src/redux/CartReducer.ts
+++ src/redux/CartReducer.ts
@@ -12,9 +12,33 @@ const cartSlice = createSlice({
             state.data = [...state.data, action.payload];
         },
         selectAttribute(state:any,action:any){
-            const selectedAttributes:{productId:string, attributes:(AttributeType & {items:ItemsToAttributeType})[]} = action.payload;
-            const productsInCart = state.data;
-            state.selectedAttributes[selectedAttributes.productId] = selectedAttributes.attributes;
+            // debugger;
+            
+            
+            const selectedAttributes:{productId:string, attributes:(AttributeType)[]} = action.payload;
+            const data = JSON.parse(JSON.stringify(state.data))
+            const product = data.filter((el:any)=>el.id === selectedAttributes.productId)[0];
+            
+            if(Array.isArray(action.payload.attributes)){
+                // debugger;
+                state.selectedAttributes[selectedAttributes.productId] = selectedAttributes.attributes;
+                return;
+            }
+            
+            
+            let indexItem:any;
+            const settedAttribute = product.attributes?.filter((el:AttributeType,index:number)=>{
+                if(el.id === (selectedAttributes as any).attributes.id){
+                    indexItem = index;
+                    return true;
+                }
+            })[0];
+            const settedAttributeItem = settedAttribute.items
+            .filter((el:ItemsToAttributeType)=> el.id === (selectedAttributes as any).attributes.items[0].id)[0];
+            
+            state.selectedAttributes[selectedAttributes.productId][indexItem].items = settedAttributeItem;
+            // debugger;
+
         }
     }
 });
