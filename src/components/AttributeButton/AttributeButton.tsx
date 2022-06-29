dd58ce2e942151141cebd3b5333fc299770c9a62
Elumion <dovgalyukvlad87@gmail.com>
2022-06-29 23:23:52 +0300

    add more logic to component

--- src/components/AttributeButton/AttributeButton.tsx
+++ src/components/AttributeButton/AttributeButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AttributeType } from "../../@types/ProductTypes";
 import { AttributeButtonContainer } from "./AttributeButton.styles";
 
 interface Props {
@@ -6,17 +7,47 @@ interface Props {
   displayValue: string;
   value: string;
   id: string;
+  isChecked: boolean;
+  parentId: string;
+  parentName: string;
+  parentType: string;
+  setAttribute: (attribute: AttributeType) => void;
 }
 
 class AttributeButton extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props);
+
+    this.state = {};
+  }
+
+  defineClassName() {
+    const typeAttribute = this.props.isSwatch ? "swatch" : "text";
+    const checkedAttribute = this.props.isChecked ? "checked" : "";
+    return `${typeAttribute} ${checkedAttribute}`;
+  }
+
+  checkAttribute(attribute: Props) {
+    const newAttribute = {
+      id: attribute.parentId,
+      name: attribute.parentName,
+      type: attribute.parentType,
+      items: [
+        {
+          id: attribute.id,
+          value: attribute.value,
+          displayValue: attribute.displayValue,
+        },
+      ],
+    };
+    this.props.setAttribute(newAttribute);
   }
 
   render() {
     return (
       <AttributeButtonContainer
-        className={this.props.isSwatch ? "swatch" : "text"}
+        onClick={this.checkAttribute.bind(this, this.props)}
+        className={this.defineClassName()}
         itemProp={this.props.value}
       >
         {!this.props.isSwatch && this.props.value}
