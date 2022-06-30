a4bec13b99f5d217a0813de63c1a9b6241b2ae66
Elumion <dovgalyukvlad87@gmail.com>
2022-06-30 19:49:49 +0300

    create component

---
+++ src/pages/ProductDescription/ProductDescription.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ProductDescription extends React.Component {
+  render() {
+    return <div>ProductDescription</div>;
+  }
+}
+
+export default ProductDescription;
