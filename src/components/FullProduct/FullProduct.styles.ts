17e2bab86b3c1c408202b8f37e6aaa1424e19c12
Elumion <dovgalyukvlad87@gmail.com>
2022-07-04 20:18:34 +0300

    refactor to reuse

--- src/components/FullProduct/FullProduct.styles.ts
+++ src/components/FullProduct/FullProduct.styles.ts
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const FullProductContainer = styled.li`
     display:flex;
     cursor: auto !important;
-    width: 325px;
+    width: 100%;
     .product__image{
         width:121px;        
         object-fit:contain;
@@ -95,5 +95,87 @@ export const FullProductContainer = styled.li`
         gap :5px;
         flex-wrap:wrap;
         align-items:flex-end;
+    }
+
+    &.full-size{
+
+        .product__info{
+            width:100%
+        }
+        .product__name{
+            font-weight: 600;
+            font-size: 30px;
+            sline-height: 27px;
+            span{
+                display:inline-block;
+                margin-top:16px;
+                font-weight: 400;
+                font-size: 30px;
+                line-height: 27px;
+            }
+        }
+
+        .price{
+            margin:20px 0;
+            font-weight: 700;
+            font-size: 24px;
+            line-height: 24px;
+
+        }
+
+        .attributes{
+            padding:0;
+            .buttons{
+                gap: 8px;
+            }
+
+            .attribute__name{
+                font-weight: 700;
+                font-size: 18px;
+                line-height: 18px;
+                text-transform:uppercase;
+            }
+        }
+        .product__image{
+            width: 200px;
+        }
+        .counter__button{
+            width:45px;
+            height:45px;
+        }
+        
+        .plus::after, .plus::before, .minus::after {
+            width:15px;
+        }
+        .arrow__rotate{
+            transform: rotate(180deg);
+        }
+
+        .img-slider__container{
+            position:relative;
+            display:flex;
+            align-items:center;
+            .slider-btns__container{
+                position:absolute;
+                bottom:16px;
+                right:16px;
+                display:flex;
+                gap:8px;
+                .slider__btn{
+                    display:flex;
+                    align-items:center;
+                    justify-content:center;
+                    width:24px;
+                    height:24px;
+                    background: rgba(0, 0, 0, 0.73);
+                    border:none;
+                    cursor:pointer;
+                    :hover{
+                        background: rgba(0, 0, 0, 0.9);
+                    }
+                }
+            }
+        }
+
     }
 `
--- src/components/FullProduct/FullProduct.tsx
+++ src/components/FullProduct/FullProduct.tsx
@@ -8,15 +8,20 @@ interface Props {
   currency: { label: string | undefined };
   checkedItems: any;
   count: number | undefined;
+  isFullSize?: boolean;
   setAttribute: (productId: string, attributes: AttributeType) => void;
   addProduct: (productId: string) => void;
   removeProduct: (productId: string) => void;
 }
 
+const arrow: string = require("../../assets/CaretLeft.svg").default;
+
 class FullProduct extends React.Component<Props, any> {
   constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedImg: this.props.product.gallery[0],
+    };
     // debugger;
   }
 
@@ -58,6 +63,7 @@ class FullProduct extends React.Component<Props, any> {
                     key={item.id}
                     value={item.value}
                     id={item.id}
+                    isFullSize={this.props.isFullSize}
                   />{" "}
                 </li>
               );
@@ -67,18 +73,37 @@ class FullProduct extends React.Component<Props, any> {
       );
     });
   }
+
+  setNextimg = () => {
+    const { gallery } = this.props.product;
+    const { selectedImg } = this.state;
+    const index = gallery.indexOf(selectedImg);
+    const nextImg = gallery[index + 1] || gallery[0];
+    this.setState({ selectedImg: nextImg });
+  };
+
+  setPrevimg = () => {
+    const { gallery } = this.props.product;
+    const { selectedImg } = this.state;
+    const index = gallery.indexOf(selectedImg);
+    const prevImg = gallery[index - 1] || gallery[gallery.length - 1];
+    this.setState({ selectedImg: prevImg });
+  };
+
   render() {
     const selectedPrice = this.props.product.prices.filter(
       (el) => el.currency.label === this.props.currency.label
     )[0];
 
     return (
-      <FullProductContainer>
-        <div className="product__info">
+      <FullProductContainer
+        className={this.props.isFullSize ? "full-size" : ""}
+      >
+        <div className={`product__info`}>
           <p className="product__name">
             {this.props.product.brand}
             <br />
-            {this.props.product.name}
+            <span>{this.props.product.name}</span>
           </p>
           <p className="price">
             {selectedPrice.currency.symbol}
@@ -99,12 +124,30 @@ class FullProduct extends React.Component<Props, any> {
             onClick={() => this.props.removeProduct(this.props.product.id)}
           ></button>
         </div>
-        <img
-          className="product__image"
-          src={this.props.product.gallery[0]}
-          alt={this.props.product.name}
-          width={121}
-        />
+        {!this.props.isFullSize ? (
+          <img
+            className="product__image"
+            src={this.props.product.gallery[0]}
+            alt={this.props.product.name}
+            width={121}
+          />
+        ) : (
+          <div className="img-slider__container">
+            <img
+              className="product__image"
+              src={this.state.selectedImg}
+              alt={this.state.selectedImg}
+            />
+            <div className="slider-btns__container">
+              <button className="slider__btn" onClick={this.setPrevimg}>
+                <img className="arrow__rotate" src={arrow} alt={arrow} />
+              </button>
+              <button className="slider__btn" onClick={this.setNextimg}>
+                <img src={arrow} alt={arrow} />
+              </button>
+            </div>
+          </div>
+        )}
       </FullProductContainer>
     );
   }
