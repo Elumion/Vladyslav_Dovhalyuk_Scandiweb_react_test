38fd32681fba6f19491d058cbeef6c675ef3aefc
Elumion <dovgalyukvlad87@gmail.com>
2022-07-14 17:44:38 +0300

    store refactor

--- src/redux/CartReducer.ts
+++ src/redux/CartReducer.ts
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-import { AttributeType, ItemsToAttributeType, ProductCardProps } from "../@types/ProductTypes";
+import { AttributeType, ItemsToAttributeType, ProductCardProps, SelectedAttributesType } from "../@types/ProductTypes";
 
 const cartSlice = createSlice({
     name:"cart",
@@ -16,78 +16,69 @@ const cartSlice = createSlice({
                 state.data = [...state.data, payload];
             }
             else{
-                // filteredArrayOfProducts.findIndex(el=>el.attributes)
+                let isHaveSimilarAttributes:boolean|undefined = false;
+                let similarAttributesProductId:number = 0;
                 for(let i = 0;i<filteredArrayOfProducts.length;i++){
-                    debugger;
-                     const isHaveSimilarAttributes = filteredArrayOfProducts[i].sellectedAttributes?.every((el,index)=>{
+                    
+                    if(!Array.isArray(filteredArrayOfProducts[i].sellectedAttributes)){
+                        let tmpArr = [];
+                        for(let key in filteredArrayOfProducts[i].sellectedAttributes){
+                            tmpArr.push( (filteredArrayOfProducts[i] as any).sellectedAttributes[key])
+                        }
+                        filteredArrayOfProducts[i].sellectedAttributes = tmpArr;
+                    }
+
+
+                     isHaveSimilarAttributes = (filteredArrayOfProducts[i] as any).sellectedAttributes.every((el:SelectedAttributesType,index:any)=>{
                         if(payload.sellectedAttributes){
                            return el.items.id === payload.sellectedAttributes[index].items.id
                         }
                         return false;
-                    }) //пройтись по вибраним атрибутам і знайти чи цей продукт має вибрані атрибути як в прийденого
+                    }) 
                     if(isHaveSimilarAttributes){
-                        const indexOfProduct:number = data.findIndex((el:ProductCardProps& {storeId:string})=>el.storeId ===filteredArrayOfProducts[i].storeId);
-                        debugger;
-                            (data[indexOfProduct] as any).count+=1;
-                        state.data = data;
-                        return;
-                        
-                    }
-                    else{
-                        debugger;
-                        const uniqid = ` ${payload.id}-${Date.now()}`;
-                        payload.storeId = uniqid;
-                        payload.count = 1;
-                        state.data = [...state.data, payload];
-                        return;
+                        similarAttributesProductId = i;
+                        break;
                     }
                 }
+                if(isHaveSimilarAttributes){
+                    const indexOfProduct:number = data.findIndex((el:ProductCardProps& {storeId:string})=>el.storeId ===filteredArrayOfProducts[similarAttributesProductId].storeId);
+                        (data[indexOfProduct] as any).count+=1;
+                    state.data = data;
+                    
+                }
+                else{
+                    const uniqid = ` ${payload.id}-${Date.now()}`;
+                    payload.storeId = uniqid;
+                    payload.count = 1;
+                    state.data = [...state.data, payload];
+                }
             }
-            // debugger;
         },
         selectAttribute(state:any,action:any){
-            const payload:{productId:string, attributes:AttributeType[]} = action.payload;
-            const data = JSON.parse(JSON.stringify(state.data));
+            const payload:{productId:string, attributes:AttributeType} = action.payload;
+            const data:(ProductCardProps)[]  = JSON.parse(JSON.stringify(state.data));
             
-            const filteredArrayOfProducts:ProductCardProps[] = data.filter((el:ProductCardProps)=>el.id === payload.productId);
-            
-            debugger;
-            
+            const productId :number = data.findIndex((el:ProductCardProps)=>el.storeId === payload.productId);
 
-
-
-
+            const {id,name,type,items} = payload.attributes;
+            const selectedAttributesArr = {
+                id,
+                name,
+                type,
+                items: items[0]
+            }
+            const changeAttributeId:number|undefined = data[productId].sellectedAttributes?.findIndex(el=>el.id === id);
+            if(typeof changeAttributeId === "number" )
+                (data[productId] as any).sellectedAttributes[changeAttributeId] = selectedAttributesArr;
             
-            // const selectedAttributes:{productId:string, attributes:(AttributeType)[]} = action.payload;
-            // const product = data.filter((el:any)=>el.id === selectedAttributes.productId)[0];
-            
-            // if(Array.isArray(action.payload.attributes)){
-            //     // debugger;
-            //     state.selectedAttributes[selectedAttributes.productId] = selectedAttributes.attributes;
-            //     return;
-            // }
-            
-            // // debugger;
-            // let indexItem:any;
-            // const settedAttribute = product.attributes?.filter((el:AttributeType,index:number)=>{
-            //     if(el.id === (selectedAttributes as any).attributes.id){
-            //         indexItem = index;
-            //         return true;
-            //     }
-            // })[0];
-            // const settedAttributeItem = settedAttribute.items
-            // .filter((el:ItemsToAttributeType)=> el.id === (selectedAttributes as any).attributes.items[0].id)[0];
-            
-            // // state.selectedAttributes[selectedAttributes.productId][indexItem].items = settedAttributeItem;
-            // state.data[]
-            // debugger;
+            state.data =data;
         },
         counterProductAdd(state:any,action:any){
             const idProduct = action.payload;
             const data = JSON.parse(JSON.stringify(state.data))
             let indexItem:any;
             const product = data.filter((el:any,index:number)=>{
-                if(el.id === idProduct){
+                if(el.storeId === idProduct){
                     indexItem = index;
                     return true;
                 }
@@ -101,7 +92,7 @@ const cartSlice = createSlice({
             const selectedAttributes = JSON.parse(JSON.stringify(state.selectedAttributes));
             let indexItem:any;
             const product = data.filter((el:any,index:number)=>{
-                if(el.id === idProduct){
+                if(el.storeId === idProduct){
                     indexItem = index;
                     return true;
                 }
